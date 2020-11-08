4d4ec69fae184c5c36ad16b7b6c64925ad82f238
Tomasz Boś <tomaszbos@gmail.com>
2020-11-08 14:36:42 +0100

    admin functionality and login, authentiication custon system

--- donations/backends.py
+++ donations/backends.py
@@ -5,11 +5,21 @@ from django.contrib.auth.backends import ModelBackend
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         user_model = get_user_model()
-        try:
-            user = user_model.objects.get(email=kwargs['email'])
-        except user_model.DoesNotExist:
+        if username is None:
+            try:
+                user = user_model.objects.get(email=kwargs['email'])
+            except user_model.DoesNotExist:
+                return None
+            else:
+                if user.check_password(password):
+                    return user
             return None
-        else:
-            if user.check_password(password):
-                return user
-        return None
+        if username is not None:
+            try:
+                user = user_model.objects.get(username=username)
+            except user_model.DoesNotExist:
+                return None
+            else:
+                if user.check_password(password):
+                    return user
+            return None
